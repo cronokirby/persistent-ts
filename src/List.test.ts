72eac11006135ebb2a80ccc9044d598b3e35b431
Lúcás Meier <cronokirby@gmail.com>
2019-06-10 11:47:21 +0200

    Add List.singleton method

--- src/List.test.ts
+++ src/List.test.ts
@@ -4,3 +4,8 @@ test('List.empty isEmpty', () => {
     const empty = List.empty();
     expect(empty.isEmpty()).toBe(true);
 });
+
+test('List.singleton is not Empty', () => {
+    const singleton = List.singleton(1);
+    expect(singleton.isEmpty()).toBe(false);
+});
--- src/List.ts
+++ src/List.ts
@@ -1,14 +1,21 @@
 type Node<T> = { empty: true } | { empty: false; value: T; next: Node<T> };
+// We can share a single empty node between all lists.
+const EMPTY_NODE = { empty: true };
 
 class List<T> {
     private constructor(private readonly _node: Node<T>) {}
 
-    isEmpty(): boolean {
-        return this._node.empty;
+    public static empty<T>(): List<T> {
+        return new List({ empty: true });
     }
 
-    static empty<T>(): List<T> {
-        return new List({ empty: true });
+    public static singleton<T>(value: T): List<T> {
+        const next = EMPTY_NODE as Node<T>;
+        return new List({ empty: false, value, next });
+    }
+
+    public isEmpty(): boolean {
+        return this._node.empty;
     }
 }
 export default List;
